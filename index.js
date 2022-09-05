16543d008879df1fe1526c7bf2fcd172fcfab8c3
edwin <ngisahedwin@gmail.com>
2022-09-05 12:13:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
+  const wordReverse = word.split("").reverse().join("");
+  if(word === wordReverse){
+    return true
+  }
+  else{
+    return false
+  }
+
   // Write your algorithm here
 }
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome("robot"))
 
 /* 
   Add your pseudocode here
@@ -8,6 +18,9 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I declared a "wordReverse" variable with parameter 'word'. i used the split method on word that splits it.
+  the reverse method reverses the letters in the string and the join method concatenates yhe reversed letters to form a string
+  I then used the if statement to set the conditions for returning if the string is a palindrone or not
 */
 
 // You can run `node index.js` to view these console logs
